4cf10a6afdbd9e2daea570107cd86a98ce748b1e
Nicolas Perez <nykolaiperez@gmail.com>
2021-09-08 12:24:54 -0300

    Proyecto hasta la clase 20

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 
 from AppCoder import views
 
+
+
+
+
 urlpatterns = [
    
-    path('', views.inicio), #esta era nuestra primer view
+    path('', views.inicio, name="Inicio"), #esta era nuestra primer view
     path('cursos', views.cursos, name="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name="Estudiantes"),
+    path('entregables', views.entregables, name="Entregables"),
 ]
 
